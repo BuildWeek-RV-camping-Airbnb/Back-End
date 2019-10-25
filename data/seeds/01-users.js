913111d665d396e13479ae45875f59b5ace8ec61
Ryan Ziegenfus <thewebcoderz@gmail.com>
2019-10-25 01:51:50 -0400

    reseeded db

--- data/seeds/01-users.js
+++ data/seeds/01-users.js
@@ -5,46 +5,107 @@ exports.seed = function(knex) {
       // Inserts seed entries
       return knex('users').insert([
   
-          {
-            "first_name": "Jessica",
-            "last_name": "Qwerty",
-            "email": "jessica@gmail.com",
-            "username": "Jessica",
-            "password": "$2a$14$R3pz5CMIviyUl1.d24kIFO8zK8vofthuoU.q.aNHkmp1ghsxeUPV2",
-            "owner": "true",
-            "avatar": ""
+        {
+          'first_name': "Clare",
+          'last_name': "Smithson",
+          'email': "clare@gmail.com",
+          'username': "CSmithson",
+          'password': "qwerty",
+          'owner': true,
+          'avatar': "https://ibb.co/Mk5JH9n"
           },
           
           {
-            "first_name": "Tom",
-            "last_name": "Qwerty",
-            "email": "tom@gmail.com",
-            "username": "Tom",
-            "password": "$2a$14$R3pz5CMIviyUl1.d24kIFO8zK8vofthuoU.q.aNHkmp1ghsxeUPV2",
-            "owner": "false",
-            "avatar": ""
+          'first_name': "Marcus",
+          'last_name': "Mason",
+          'email': "marcus@gmail.com",
+          'username': "MMason",
+          'password': "qwerty",
+          'owner': true,
+          'avatar': "https://ibb.co/0JrgYff"
           },
-
+          
+          
+          
           {
-            "first_name": "Lee",
-            "last_name": "Qwerty",
-            "email": "lee@gmail.com",
-            "username": "Lee",
-            "password": "$2a$14$R3pz5CMIviyUl1.d24kIFO8zK8vofthuoU.q.aNHkmp1ghsxeUPV2",
-            "owner": "false",
-            "avatar": ""
+          'first_name': "Jason",
+          'last_name': "LaFlamme",
+          'email': "jason@gmail.com",
+          'username': "JLaFlamme",
+          'password': "qwerty",
+          'owner': true,
+          'avatar': "https://ibb.co/HDJsLzJ"
           },
-
+          
           {
-            "first_name": "Frank",
-            "last_name": "Qwerty",
-            "email": "frank@gmail.com",
-            "username": "Frank",
-            "password": "$2a$14$R3pz5CMIviyUl1.d24kIFO8zK8vofthuoU.q.aNHkmp1ghsxeUPV2",
-            "owner": "true",
-            "avatar": ""
+          'first_name': "Tina",
+          'last_name': "Charles",
+          'email': "tina@gmail.com",
+          'username': "TCharles",
+          'password': "qwerty",
+          'owner': true,
+          'avatar': "https://ibb.co/tbd9mmb"
           },
-
+          
+          {
+          'first_name': "Mama",
+          'last_name': "Lopez",
+          'email': "mama@gmail.com",
+          'username': "Mama",
+          'password': "qwerty",
+          'owner': true,
+          'avatar': "https://ibb.co/dpxWLsc"
+          },
+          
+          {
+          'first_name': "Matty",
+          'last_name': "Sloan",
+          'email': "matty@gmail.com",
+          'username': "MSloan",
+          'password': "qwerty",
+          'owner': true,
+          'avatar': "https://ibb.co/4SGn2Hv"
+          },
+          
+          {
+          'first_name': "David",
+          'last_name': "Mumm",
+          'email': "david@gmail.com",
+          'username': "DMumm",
+          'password': "qwerty",
+          'owner': true,
+          'avatar': "https://ibb.co/YcVg0nP"
+          },
+          
+          {
+          'first_name': "Ajit",
+          'last_name': "Khan",
+          'email': "ajit@gmail.com",
+          'username': "AKhan",
+          'password': "qwerty",
+          'owner': true,
+          'avatar': "https://ibb.co/jVPN3NL"
+          },
+          
+          {
+          'first_name': "Hana",
+          'last_name': "Stone",
+          'email': "hana@gmail.com",
+          'username': "HStone",
+          'password': "qwerty",
+          'owner': true,
+          'avatar': "https://ibb.co/kqL15v5"
+          },
+          
+          {
+          'first_name': "Coco",
+          'last_name': "Pérez",
+          'email': "coco@gmail.com",
+          'username': "CPerez",
+          'password': "qwerty",
+          'owner': true,
+          'avatar': "https://ibb.co/DkhSpQM"
+          },
 
       ]);
     // });
